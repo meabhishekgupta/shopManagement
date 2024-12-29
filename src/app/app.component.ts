cd0c32fa9a51245fde61ba688ecfec123689f713
meabhishekgupta <abhi5048462@gmail.com>
2024-12-30 02:37:05 +0530

    Created Routes and components
    Created Routes and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShopsComponent } from './shops/shops.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,HomeComponent,ShopsComponent,AboutComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { ShopsComponent } from './shops/shops.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',    
+        redirectTo: 'Home',
+        pathMatch: 'full'
+    },
+    {
+        path:'Shops',
+        component: ShopsComponent
+    },
+    {
+        path:'Home',
+        component: HomeComponent
+    },
+    {
+        path:'About',
+        component: AboutComponent
+    },
+    {
+        path:'Contact',
+        component: ContactComponent
+    }
+];
